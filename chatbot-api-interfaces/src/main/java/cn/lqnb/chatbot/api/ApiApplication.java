0ec52fa6068241764bd4fa4bd7b8b699eba0a679
liqindddd <123456>
2023-12-13 15:35:46 +0800

    feat: init ddd project

--- chatbot-api-interfaces/src/main/java/cn/lqnb/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/cn/lqnb/chatbot/api/ApiApplication.java
@@ -1,14 +1,18 @@
 package cn.lqnb.chatbot.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @description: 启动入口
  * @author：遇见你
  * @date: 2023-12-13
  * @Copyright： 只要学不死，就往死里学。
  */
+@SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
